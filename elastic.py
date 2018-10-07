89121b42fa1edff2891426c9452392cd3409359b
proteeti13 <proteeti13@gmail.com>
2018-10-08 00:14:26 +0600

    added elasticsearch

---
+++ elastic.py
@@ -0,0 +1,12 @@
+import logging
+from elasticsearch import Elasticsearch
+_es = None
+
+
+def connect_elasticsearch():
+    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    if _es.ping():
+        print('Yay Connect')
+    else:
+        print('Awww it could not connect!')
+    return _es
